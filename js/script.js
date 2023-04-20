8c3a25cac7d1b72bc781bf3800df5c65a818f0e5
raFa <rafalimabcl@gmail.com>
2023-04-20 17:12:05 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const dropArea = document.querySelector('.drag-files');
+
+
+dropArea.addEventListener('dragover', () => {
+    dropArea.classList.add('dragover')
+})
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('dragover')
+})
